a4c8e794252f091567f0cc187953a1c8e215b2e6
Lorenzo Mazzarotto <80178195+tcllmazza@users.noreply.github.com>
2021-03-31 11:26:31 -0300

    Add files via upload

---
+++ 30 Days of Code/Day 01; Data Types/Contents.swift
@@ -0,0 +1,22 @@
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+// Declare second integer, double, and String variables.
+var otherInteger: Int
+var otherDouble: Double
+var otherString: String
+
+// Read and save an integer, double, and String to your variables.
+otherInteger = Int(readLine()!)!
+otherDouble = Double(readLine()!)!
+otherString = readLine()!
+
+// Print the sum of both integer variables on a new line.
+print(i + otherInteger)
+
+// Print the sum of the double variables on a new line.
+print(d + otherDouble)
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print(s + otherString)
