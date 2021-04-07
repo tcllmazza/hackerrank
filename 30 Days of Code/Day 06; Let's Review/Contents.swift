914c98ea1b02e119fdd4bebe45c2edc4c336d8e0
Lorenzo Mazzarotto <80178195+tcmzrt@users.noreply.github.com>
2021-04-07 03:02:29 -0300

    Add files via upload

---
+++ 30 Days of Code/Day 06; Let's Review/Contents.swift
@@ -0,0 +1,36 @@
+import Foundation
+import Darwin
+
+let numStrings = Int(readLine()!)!
+
+func printEvenAndOdd(string: String) {
+    // This prints inputString to stderr for debugging:
+    fputs("string: " + string + "\n", stderr)
+    
+    // Print the even-indexed characters
+    // Write your code here
+    for (index, char) in string.enumerated() {
+        if index % 2 == 0 {
+            print(char, terminator: "")
+        }
+    }
+    
+    // Print a space
+    print(" ", terminator: "")
+    
+    // Print the odd-indexed characters
+    // Write your code here
+    for (index, char) in string.enumerated() {
+        if index % 2 != 0 {
+            print(char, terminator: "")
+        }
+    }
+    
+    // Print a newline
+    print("")
+}
+
+for _ in 1...numStrings {
+    let inputString = readLine()!
+    printEvenAndOdd(string: inputString)
+}
