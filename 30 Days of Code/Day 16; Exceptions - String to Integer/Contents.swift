326653c8318055163c40f35b9cb154866dad846a
Lorenzo Mazzarotto <80178195+tcmzrt@users.noreply.github.com>
2021-04-20 02:35:49 -0300

    Add files via upload

---
+++ 30 Days of Code/Day 16; Exceptions - String to Integer/Contents.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+/*
+ * Define an ErrorType
+ */
+enum StringToIntTypecastingError: Error {
+    case BadString
+}
+
+/*
+ * If typecasting is not possible, throw exception, otherwise return the Integer value
+ */
+func stringToInt(inputString: String) throws -> Int {
+    // Write your code here
+    guard let number = Int(inputString) else {
+        throw StringToIntTypecastingError.BadString
+    }
+
+    return number
+}
+
+/*
+ * Read the input
+ */
+let inputString = readLine()!
+
+do {
+    try print(stringToInt(inputString: inputString))
+} catch StringToIntTypecastingError.BadString {
+    print("Bad String")
+}
